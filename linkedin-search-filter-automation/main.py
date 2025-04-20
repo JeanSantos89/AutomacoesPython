ab7e0cfb501959be9e8267f88b49349f83659e7e
Jean Santos <jeansaantos89@gmail.com>
2025-04-20 15:58:53 -0300

    20/04

--- linkedin-search-filter-automation/main.py
+++ linkedin-search-filter-automation/main.py
@@ -19,19 +19,45 @@ driver = webdriver.Chrome(options=options)
 
 # Abre o Site com a palvra chave dentro do link de pesquisa
 driver.get(f"https://www.linkedin.com/search/results/all/?keywords={PalavraChave}&origin=GLOBAL_SEARCH_HEADER&sid=z.y")  # já logado se o perfil estiver certo!
-time.sleep(10)
+time.sleep(3)
 
 # Clica no botão pessoas
 driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div[2]/div/div[1]/div/div/div/section/ul/li[3]/button').click()
-time.sleep(10)
+time.sleep(2)
 
-driver.find_element(By.XPATH, '//*[@id="jLq9i4rBRK6D3/UEqWF7Qg=="]/div/div[2]/a').click()
-time.sleep(10)
+# Ver todos os resultados
+driver.find_element(By.PARTIAL_LINK_TEXT, "Ver todos os resultados de pessoas").click()
+time.sleep(2)
+
+# Filtro localidade
+driver.find_element(By.CSS_SELECTOR, "#searchFilter_geoUrn").click()
+time.sleep(2)
+
+# Colocar cidade
+driver.find_element(By.CSS_SELECTOR, 'input.basic-input[aria-label="Adicionar localidade"]').click()
+driver.find_element(By.CSS_SELECTOR, 'input.basic-input[aria-label="Adicionar localidade"]').send_keys("Ponta Grossa, Paraná")
+time.sleep(2)
+# Aguarda aparecer a sugestão no popup
+sugestao = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.basic-typeahead__selectable[role="option"]'))
+)
+sugestao.click()
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, 'button#ember304').click()
+time.sleep(2)
+
+
+#driver.find_element(By.CSS_SELECTOR, '.basic-typehead__triggered-content ').click()
+#elemento = driver.find_element(By.CSS_SELECTOR, 'div.basic-typehead__triggered_content[role="listbox"]')
+#elemento.click()
+#time.sleep(7)
+
+
 
 #driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div[2]/div/div[1]/main/div/div/div[5]/div/div[2]/a').click()
 #time.sleep(10)
-botao = driver.find_element(By.XPATH, "//button[contains(text(), 'Ver todos os resultados de pessoas')]")
-botao.click()
+#botao = driver.find_element(By.XPATH, "//button[contains(text(), 'Ver todos os resultados de pessoas')]")
+#botao.click()
 
 
 
@@ -44,15 +70,15 @@ botao.click()
 
 
 
-driver.find_element(By.XPATH, '//*[@id="artdeco-hoverable-artdeco-gen-83"]/div[1]/div/form/fieldset/div[1]/div/div/input').send_keys("Ponta Grossa, Paraná, Brasil")
-WebDriverWait(driver, 2).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="artdeco-hoverable-artdeco-gen-83"]/div[1]/div/form/fieldset/div[1]/ul/li[1]/label/p')) # Expected espera receber uma tupla, por isso, dois parênteses
-)
-driver.find_element(By.XPATH, '//*[@id="artdeco-hoverable-artdeco-gen-83"]/div[1]/div/form/fieldset/div[1]/ul/li[1]/label/p').click()
-WebDriverWait(driver, 2).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="ember495"]/span')) # Expected espera receber uma tupla, por isso, dois parênteses
-)
-driver.find_element(By.XPATH, '//*[@id="ember495"]/span').click()
-time.sleep(3)
+#driver.find_element(By.XPATH, '//*[@id="artdeco-hoverable-artdeco-gen-83"]/div[1]/div/form/fieldset/div[1]/div/div/input').send_keys("Ponta Grossa, Paraná, Brasil")
+#WebDriverWait(driver, 2).until(
+#    EC.element_to_be_clickable((By.XPATH, '//*[@id="artdeco-hoverable-artdeco-gen-83"]/div[1]/div/form/fieldset/div[1]/ul/li[1]/label/p')) # Expected espera receber uma tupla, por isso, dois parênteses
+#)
+#driver.find_element(By.XPATH, '//*[@id="artdeco-hoverable-artdeco-gen-83"]/div[1]/div/form/fieldset/div[1]/ul/li[1]/label/p').click()
+#WebDriverWait(driver, 2).until(
+#    EC.element_to_be_clickable((By.XPATH, '//*[@id="ember495"]/span')) # Expected espera receber uma tupla, por isso, dois parênteses
+##)
+#driver.find_element(By.XPATH, '//*[@id="ember495"]/span').click()
+#time.sleep(3)
 
 

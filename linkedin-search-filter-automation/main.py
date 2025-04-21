7f9f7f6e3b43ba0b3e3bceac8c88d926a674fe35
Jean Santos <jeansaantos89@gmail.com>
2025-04-21 15:23:40 -0300

    21-04

--- linkedin-search-filter-automation/main.py
+++ linkedin-search-filter-automation/main.py
@@ -19,19 +19,19 @@ driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 10)
 # Abre o Site com a palvra chave dentro do link de pesquisa
 driver.get(f"https://www.linkedin.com/search/results/all/?keywords={PalavraChave}&origin=GLOBAL_SEARCH_HEADER&sid=z.y")  # já logado se o perfil estiver certo!
-time.sleep(3)
+time.sleep(5)
 
 # Clica no botão pessoas
 driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div[2]/div/div[1]/div/div/div/section/ul/li[3]/button').click()
-time.sleep(2)
+time.sleep(5)
 
 # Ver todos os resultados
 driver.find_element(By.PARTIAL_LINK_TEXT, "Ver todos os resultados de pessoas").click()
-time.sleep(2)
+time.sleep(5)
 
 # Filtro localidade
 driver.find_element(By.CSS_SELECTOR, "#searchFilter_geoUrn").click()
-time.sleep(2)
+time.sleep(5)
 
 # Colocar cidade
 driver.find_element(By.CSS_SELECTOR, 'input.basic-input[aria-label="Adicionar localidade"]').click()
@@ -42,23 +42,6 @@ sugestao = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.basic-typeahead__selectable[role="option"]'))
 )
 sugestao.click()
-time.sleep(5)
-# Aplicar filtro
-driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div[2]/section/div/nav/div/ul/li[5]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]').click()
-time.sleep(5)
-
-
-# Filtro Empresa Atual
-driver.find_element(By.CSS_SELECTOR, "#searchFilter_currentCompany").click()
-time.sleep(2)
-
-# Colocar empresa
-#driver.find_element(By.CSS_SELECTOR, 'input.basic-input[aria-label="Adicionar empresa"]').click()
-#time.sleep(5)
-SelecionaEmpresa = wait.until(EC.element_to_be_clickable(
-    (By.XPATH, f'//span[normalize-space()="{PalavraChave}"]')
-))
-SelecionaEmpresa.click()
 time.sleep(5)
 # Aplicar filtro
 driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div[2]/section/div/nav/div/ul/li[5]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]').click()
